e55527dd0f078633b0de03ac442121ef0498dbb7
KromaStone <groot707cod@gmail.com>
2024-02-06 09:33:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SupportComponent } from './support/support.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SupportComponent,
+    ContactComponent,
+    EmployeeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
